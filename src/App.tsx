903d3650a54e74b16d44cf71d90bbfbafea8a478
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-10-18 14:42:34 -0300

    added the Layout component and children in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import { useState } from "react";
+import { Layout } from "./components";
 
-function App() {
-  return <h1>Hello World</h1>;
-}
+const App = () => {
+  return (
+    <main>
+      <Layout>
+        <div>
+          <img
+            src="https://avatars.githubusercontent.com/u/54115624?v=4"
+            alt="Avatar of user"
+          />
+          <h1>José Eduardo Rodrigues Pinto</h1>
+          <h3>Username: </h3>
+          <span>jerp86</span>
+
+          <div>
+            <h4>Followers</h4>
+            <span>5</span>
+          </div>
+
+          <div>
+            <h4>Followings</h4>
+            <span>5</span>
+          </div>
+
+          <div>
+            <h4>Starreds</h4>
+            <span>5</span>
+          </div>
+        </div>
+        <div>Repositories</div>
+        <div>Starreds</div>
+      </Layout>
+    </main>
+  );
+};
 
 export default App;
