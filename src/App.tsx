8f7bca80df38df83e38bc6f4ff489453e9b6e2cf
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-10-18 16:06:28 -0300

    resetCSS created and applied in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Layout, Profile } from "./components";
+import { ResetCSS } from "./global";
 
 const App = () => {
   return (
     <main>
+      <ResetCSS />
       <Layout>
         <Profile />
 
