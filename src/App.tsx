2dfc383e8578e1e8f3b3f1dc4693688151afc463
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-10-18 15:09:58 -0300

    added the Profile component in the App

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
-import { Layout } from "./components";
+import { Layout, Profile } from "./components";
 
 const App = () => {
   return (
     <main>
       <Layout>
-        <div>
-          <img
-            src="https://avatars.githubusercontent.com/u/54115624?v=4"
-            alt="Avatar of user"
-          />
-          <h1>José Eduardo Rodrigues Pinto</h1>
-          <h3>Username: </h3>
-          <span>jerp86</span>
+        <Profile />
 
-          <div>
-            <h4>Followers</h4>
-            <span>5</span>
-          </div>
-
-          <div>
-            <h4>Followings</h4>
-            <span>5</span>
-          </div>
-
-          <div>
-            <h4>Starreds</h4>
-            <span>5</span>
-          </div>
-        </div>
         <div>Repositories</div>
         <div>Starreds</div>
       </Layout>
