986413239ee483aeac4bb57fe0ff9ce4e68338aa
José Eduardo Rodrigues Pinto Jerp86 <jerp4@hotmail.com>
2021-11-22 20:22:59 -0300

    Layout: added logo image

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -7,6 +7,11 @@ import { ContainerLayout } from "./styles";
 const Layout: React.FC = ({ children }) => {
   return (
     <ContainerLayout>
+      <img
+        src="https://user-images.githubusercontent.com/54115624/101699995-0bd6e600-3a5b-11eb-8c2a-6c305ab1ebfe.png"
+        alt="DIO logo"
+        loading="lazy"
+      />
       <Header />
       {children}
     </ContainerLayout>
--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -3,5 +3,15 @@ import styled from "styled-components";
 import { pxToRem } from "../../utils";
 
 export const ContainerLayout = styled.section`
-  margin: ${pxToRem(16)};
+  margin: ${pxToRem(16)} auto;
+  max-width: 960px;
+
+  & > img {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: ${pxToRem(32)} auto;
+    width: fit-content;
+    height: ${pxToRem(100)};
+  }
 `;
